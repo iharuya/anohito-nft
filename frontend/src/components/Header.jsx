45009b8a0b86d08dc5c58c1f6631add4215adbb4
iharuya <i4haruya@gmail.com>
2022-09-27 20:32:56 +0900

    add external links on the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { Link, NavLink } from "react-router-dom"
 import { ConnectKitButton } from "connectkit"
+const etherscanUrl = import.meta.env.VITE_ETHERSCAN_URL
+const openseaUrl = import.meta.env.VITE_OPENSEA_URL
 
 const verticalSpacerStyle = {
   width: "2px",
@@ -17,11 +19,11 @@ export const Header = () => {
             <div>
               <Link to="/" className="navbar-brand d-flex align-items-center">
                 <img
-                  src="/images/icon.ico"
+                  src="/images/icon.png"
                   alt="Logo"
-                  width="64"
-                  height="64"
-                  className="d-inline-block align-text-top"
+                  width="48"
+                  height="48"
+                  className="d-inline-block"
                 />
                 <span style={{ fontFamily: "Pacifico" }} className="text-reset d-none d-md-inline">
                   YOU-KNOW-WHO
@@ -51,6 +53,16 @@ export const Header = () => {
                 <div style={verticalSpacerStyle} className="d-none d-md-block"></div>
                 <li className="nav-item d-none d-md-block">
                   <ConnectKitButton label="ウォレットを接続" showBalance={true} />
+                </li>
+                <li className="nav-item d-flex align-items-center ps-3">
+                  <a href={etherscanUrl} target="_blank">
+                    <img src="/public/images/etherscan-logo.webp" width={32} height={32} />
+                  </a>
+                </li>
+                <li className="nav-item d-flex align-items-center ps-3">
+                  <a href={openseaUrl} target="_blank">
+                    <img src="/public/images/opensea-logo.png" width={32} height={32} />
+                  </a>
                 </li>
               </ul>
             </div>
